9fa0aa7d1a05b9edb90443d29d72d7f9dea7dd94
Ashutos sahoo <ashutossahoo3984@gmail.com>
2024-03-05 19:57:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,44 +3,28 @@ import os
 import sys
 import boto3
 import streamlit as st
-
-## We will be suing Titan Embeddings Model To generate Embedding
-
 from langchain_community.embeddings import BedrockEmbeddings
 from langchain.llms.bedrock import Bedrock
-
-## Data Ingestion
-
 import numpy as np
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-
-# Vector Embedding And Vector Store
-
 from langchain.vectorstores import FAISS
-
-## LLm Models
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 
-## Bedrock Clients
 bedrock=boto3.client(service_name="bedrock-runtime")
 bedrock_embeddings=BedrockEmbeddings(model_id="amazon.titan-embed-text-v1",client=bedrock)
 
-
-## Data ingestion
-def data_ingestion():
+def loading_data():
     loader=PyPDFDirectoryLoader("data")
     documents=loader.load()
-
-    # - in our testing Character split works better with this PDF data set
     text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000,
                                                  chunk_overlap=1000)
     
     docs=text_splitter.split_documents(documents)
     return docs
 
-## Vector Embedding and vector store
+
 
 def get_vector_store(docs):
     vectorstore_faiss=FAISS.from_documents(
@@ -50,14 +34,12 @@ def get_vector_store(docs):
     vectorstore_faiss.save_local("faiss_index")
 
 def get_claude_llm():
-    ##create the Anthropic Model
     llm=Bedrock(model_id="ai21.j2-mid-v1",client=bedrock,
                 model_kwargs={'maxTokens':512})
     
     return llm
 
 def get_llama2_llm():
-    ##create the Anthropic Model
     llm=Bedrock(model_id="meta.llama2-70b-chat-v1",client=bedrock,
                 model_kwargs={'max_gen_len':512})
     
@@ -98,7 +80,7 @@ def get_response_llm(llm,vectorstore_faiss,query):
 def main():
     st.set_page_config("Chat PDF")
     
-    st.header("Chat with PDF using AWS Bedrock💁")
+    st.header("Chat with PDF using AWS Bedrock")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
@@ -107,7 +89,7 @@ def main():
         
         if st.button("Vectors Update"):
             with st.spinner("Processing..."):
-                docs = data_ingestion()
+                docs = loading_data()
                 get_vector_store(docs)
                 st.success("Done")
 
